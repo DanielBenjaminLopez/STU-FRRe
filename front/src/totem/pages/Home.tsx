6a4b20d34ed4f6afea9155600c37cc8d416fd4d6
Tomas Agustin Lopez <111098118+tomas-lp@users.noreply.github.com>
2026-06-05 16:07:06 -0300

    Agrega pantalla inicial con encabezado y grid vacio

--- front/src/totem/pages/Home.tsx
+++ front/src/totem/pages/Home.tsx
@@ -1,7 +1,17 @@
+import Encabezado from "../../shared/components/widgets/Encabezado";
+
 export default function Home() {
   return (
-    <div className="home">
-      <h1>Totem Home</h1>
+    <div className="flex flex-col max-w-270 h-480 mx-auto border-x border-gray-200 p-16 gap-16">
+      <Encabezado />
+
+      <div className="flex flex-col grow">
+        <div className="grid grid-cols-4 gap-4 grid-rows-6 w-full h-full">
+          {Array.from({ length: 24 }).map((_, index) => (
+            <div key={index} className="bg-gray-100 rounded-4xl" />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
