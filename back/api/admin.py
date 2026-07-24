791405c574c6fa699db70c751a8e9098acb24644
AgustinMartinez7 <agustindago100@gmail.com>
2026-07-24 01:33:00 -0300

    Agrega libreria para cargar datos de manera automatica con el admin de django

--- back/api/admin.py
+++ back/api/admin.py
@@ -1,90 +1,142 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
 
 from .models import (
-    Totem,
-    Espacio,
-    Carrera,
-    Materia,
-    PlanMateria,
-    Comision,
-    HorarioCursado,
-    MesaExamen,
-    Evento,
     Aviso,
+    Carrera,
+    Comision,
+    Espacio,
+    Evento,
+    HorarioCursado,
+    Materia,
+    MesaExamen,
     Noticias,
+    PlanMateria,
+    Totem,
+)
+from .resources import (
+    AvisoResource,
+    CarreraResource,
+    ComisionResource,
+    EspacioResource,
+    EventoResource,
+    HorarioCursadoResource,
+    MateriaResource,
+    MesaExamenResource,
+    NoticiasResource,
+    PlanMateriaResource,
+    TotemResource,
 )
 
 
 @admin.register(Totem)
-class TotemAdmin(admin.ModelAdmin):
+class TotemAdmin(ImportExportModelAdmin):
+    resource_class = TotemResource
     list_display = ['nombre', 'espacio', 'creado_en']
     search_fields = ['nombre']
 
 
 @admin.register(Espacio)
-class EspacioAdmin(admin.ModelAdmin):
+class EspacioAdmin(ImportExportModelAdmin):
+    resource_class = EspacioResource
     list_display = ['nombre', 'tipo', 'piso']
     list_filter = ['tipo', 'piso']
     search_fields = ['nombre']
 
 
 @admin.register(Carrera)
-class CarreraAdmin(admin.ModelAdmin):
+class CarreraAdmin(ImportExportModelAdmin):
+    resource_class = CarreraResource
     list_display = ['nombre']
     search_fields = ['nombre']
 
 
 @admin.register(Materia)
-class MateriaAdmin(admin.ModelAdmin):
+class MateriaAdmin(ImportExportModelAdmin):
+    resource_class = MateriaResource
     list_display = ['nombre']
     search_fields = ['nombre']
 
 
 @admin.register(PlanMateria)
-class PlanMateriaAdmin(admin.ModelAdmin):
-    list_display = ['carrera', 'materia', 'nivel', 'modalidad', 'cuatrimestre', 'plan_estudio']
+class PlanMateriaAdmin(ImportExportModelAdmin):
+    resource_class = PlanMateriaResource
+    list_display = [
+        'carrera',
+        'materia',
+        'nivel',
+        'modalidad',
+        'cuatrimestre',
+        'plan_estudio',
+    ]
     list_filter = ['carrera', 'nivel', 'modalidad', 'plan_estudio']
 
 
 @admin.register(Comision)
-class ComisionAdmin(admin.ModelAdmin):
+class ComisionAdmin(ImportExportModelAdmin):
+    resource_class = ComisionResource
     list_display = ['nombre', 'plan_materia']
     list_filter = ['plan_materia__carrera']
     search_fields = ['nombre', 'plan_materia__materia__nombre']
 
 
 @admin.register(HorarioCursado)
-class HorarioCursadoAdmin(admin.ModelAdmin):
+class HorarioCursadoAdmin(ImportExportModelAdmin):
+    resource_class = HorarioCursadoResource
     list_display = [
-        'comision', 'espacio', 'dia_semana',
-        'hora_inicio', 'hora_fin', 'activo',
+        'comision',
+        'espacio',
+        'dia_semana',
+        'hora_inicio',
+        'hora_fin',
+        'activo',
     ]
     list_filter = ['dia_semana', 'activo']
-    search_fields = ['comision__nombre', 'comision__plan_materia__materia__nombre']
+    search_fields = [
+        'comision__nombre',
+        'comision__plan_materia__materia__nombre',
+    ]
 
 
 @admin.register(MesaExamen)
-class MesaExamenAdmin(admin.ModelAdmin):
-    list_display = ['plan_materia', 'espacio', 'fecha', 'hora', 'turno', 'activo']
+class MesaExamenAdmin(ImportExportModelAdmin):
+    resource_class = MesaExamenResource
+    list_display = [
+        'plan_materia',
+        'espacio',
+        'fecha',
+        'hora',
+        'turno',
+        'activo',
+    ]
     list_filter = ['turno', 'activo', 'plan_materia__carrera']
     search_fields = ['plan_materia__materia__nombre']
 
 
 @admin.register(Evento)
-class EventoAdmin(admin.ModelAdmin):
-    list_display = ['titulo', 'tipo', 'fecha_hora_inicio', 'fecha_hora_fin', 'espacio']
+class EventoAdmin(ImportExportModelAdmin):
+    resource_class = EventoResource
+    list_display = [
+        'titulo',
+        'tipo',
+        'fecha_hora_inicio',
+        'fecha_hora_fin',
+        'espacio',
+    ]
     list_filter = ['tipo']
     search_fields = ['titulo']
 
 
 @admin.register(Aviso)
-class AvisoAdmin(admin.ModelAdmin):
+class AvisoAdmin(ImportExportModelAdmin):
+    resource_class = AvisoResource
     list_display = ['horario_cursado', 'evento', 'fecha', 'motivo', 'tipo']
     list_filter = ['tipo']
     search_fields = ['motivo']
 
 
 @admin.register(Noticias)
-class NoticiasAdmin(admin.ModelAdmin):
+class NoticiasAdmin(ImportExportModelAdmin):
+    resource_class = NoticiasResource
     list_display = ['titulo', 'fecha_publicacion', 'fecha_expiracion']
     search_fields = ['titulo', 'contenido']
